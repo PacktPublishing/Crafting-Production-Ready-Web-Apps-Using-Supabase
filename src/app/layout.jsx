bd8dbe4d19b6543d2b76e193df46a21f23e353b1
David Lorenz <david@wahnsinn.design>
2023-09-13 12:25:58 +0200

    app router changes

---
+++ src/app/layout.jsx
@@ -0,0 +1,18 @@
+import "@/styles/globals.scss";
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "My App Page Title",
+};
+
+export default function Page() {
+  return <div>Hello App Page</div>;
+}
