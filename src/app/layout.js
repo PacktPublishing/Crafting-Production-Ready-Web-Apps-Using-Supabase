e423855d9c5aa0d1884df28a5e58d3b134b1f22f
David Lorenz <david@wahnsinn.design>
2023-10-10 17:40:10 +0200

    added pico.css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,16 @@
 import "@/styles/globals.scss";
 import SupabaseProvider from "./SupabaseProvider";
 
-export default function RootLayout(pageProps) {
-  console.log("@pageProps", pageProps);
+export default async function RootLayout(pageProps) {
   return (
     <html lang="en">
       <head>
         {/* ... */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css"
+        ></link>
       </head>
       <body>
         <SupabaseProvider>
