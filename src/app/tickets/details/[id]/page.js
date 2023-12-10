47cca3e794eeaaa65b69830a1d5a186c40868155
David Lorenz <david@wahnsinn.design>
2023-12-10 17:18:08 +0100

    details page improved

--- src/app/tickets/details/[id]/page.js
+++ src/app/tickets/details/[id]/page.js
@@ -1,7 +1,27 @@
 export default function TicketDetailsPage({ params }) {
   return (
-    <div>
-      Ticket Details page with <strong>ID={params.id}</strong>
-    </div>
+    <article>
+      <header>
+        <strong>#{params.id}</strong> - <strong>Open</strong>
+        <br />
+        <small>Created by Author</small>
+        <h2>Ticket title should be here</h2>
+      </header>
+
+      <section>Some details about the ticket should be here.</section>
+
+      <article>
+        <h3>Comments</h3>
+
+        <textarea placeholder="Add a comment"></textarea>
+        <button>Add comment</button>
+
+        <section>
+          <p>Newest Comment</p>
+          <p>Older comment</p>
+          <p>Oldest comment</p>
+        </section>
+      </article>
+    </article>
   );
 }
