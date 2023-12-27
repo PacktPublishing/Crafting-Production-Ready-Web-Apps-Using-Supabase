a493e69cad0a4bda064a2b64ecdbba9a4a2f32e1
David Lorenz <david@wahnsinn.design>
2023-12-27 10:54:26 +0100

    use rlist added

--- src/app/tickets/users/page.js
+++ src/app/tickets/users/page.js
@@ -1,3 +1,40 @@
 export default function UserList() {
-  return <div>This is the page for the User List</div>;
+  const users = [
+    {
+      name: "Alice Ling",
+      job: "Software Engineer",
+      isAvailable: false,
+    },
+    {
+      name: "Harry Green",
+      job: "QA Engineer",
+      isAvailable: false,
+    },
+    {
+      name: "Trudy Torres",
+      job: "Project Manager",
+      isAvailable: true,
+    },
+  ];
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.name}>
+            <td>{user.name}</td>
+            <td>{user.job}</td>
+            <td>{user.isAvailable ? "Available" : "Not available"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
