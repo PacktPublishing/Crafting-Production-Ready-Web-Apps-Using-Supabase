40e0c437f3c7d7786d5223e7ba04bde8e664588c
David Lorenz <david@wahnsinn.design>
2024-01-05 15:14:17 +0100

    undone auth session middleware

--- src/middleware.js
+++ src/middleware.js
@@ -4,6 +4,6 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   const res = NextResponse.next();
   const supabase = createReqResSupabase({ req, res });
-  await supabase.auth.getSession();
+
   return res;
 }
