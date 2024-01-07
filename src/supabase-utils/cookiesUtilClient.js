975ac378f49a8151ccf30d6f32cef524bf3f3304
David Lorenz <david@wahnsinn.design>
2024-01-07 20:52:03 +0100

    fixing the utils

--- src/supabase-utils/cookiesUtilClient.js
+++ src/supabase-utils/cookiesUtilClient.js
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 export const createCookiesUtilSupabase = () => {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
         get: (name) => cookies().get(name)?.value,
