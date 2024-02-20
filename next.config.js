b025703fb0fa83f0fb42c23abcc8af74e7b8c251
David Lorenz <info@activenode.de>
2024-02-20 14:09:57 +0100

    fixing the next.config file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => [
+  redirects: async () => [
     {
       source: "/logout",
       destination: "/auth/logout",
+      permanent: true,
     },
   ],
 };
