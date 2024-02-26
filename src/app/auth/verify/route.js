f4fab11f3a9ed9ef6c3cef23796f7e7d31341a2e
David Lorenz <info@activenode.de>
2024-02-26 10:47:46 +0100

    fixed verify

--- src/app/auth/verify/route.js
+++ src/app/auth/verify/route.js
@@ -7,19 +7,23 @@ export async function GET(request) {
   const isRecovery = searchParams.get("type") === "recovery";
 
   const supabase = getSupabaseCookiesUtilClient();
+
+  let verifyType = "magiclink";
+  if (isRecovery) verifyType = "recovery";
+
   const { error } = await supabase.auth.verifyOtp({
-    type: "magiclink",
+    type: verifyType,
     token_hash: hashed_token,
   });
 
   if (error) {
     return NextResponse.redirect(
-      new URL("/error?type=invalid_magiclink", request.url)
+      new URL("/error?type=invalid_magiclink", request.url),
     );
   } else {
     if (isRecovery) {
       return NextResponse.redirect(
-        new URL("/tickets/change-password", request.url)
+        new URL("/tickets/change-password", request.url),
       );
     } else {
       return NextResponse.redirect(new URL("/tickets", request.url));
